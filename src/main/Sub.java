eedf192415e34fdbc8cdab8de4410b3914236e09
byung8052 <55075492+byung8052@users.noreply.github.com>
2020-07-17 17:01:19 +0900

    연산 프로그램

--- src/main/Sub.java
+++ src/main/Sub.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Sub {
 	public void subnum() {
+		int tries = 0;
 		int clear = 0;
 		int fails = 0;
 
@@ -14,13 +15,13 @@ public class Sub {
 			Random rnd = new Random();
 			int a = (rnd.nextInt(80)+10);
 			int b = (rnd.nextInt(80)+10);
-			double c = a-b; // -로 나올 수 있기 때문에 int가 아닌 double
+			double c = a-b; //음수가 될 수 있기 떄문에 int가 아닌 double
 			
 		try{
 			Scanner sc = new Scanner(System.in);
 			System.out.println(a+"-"+b);
 			System.out.println("정답?: ");
-			double d = sc.nextInt(); // -로 나올 수 있기 때문에 int가 아닌 double
+			double d = sc.nextInt(); //음수가 될 수 있기 떄문에 int가 아닌 double
 			
 			if(c == d) {
 				System.out.println("정답");
@@ -34,7 +35,6 @@ public class Sub {
 			System.out.println("오류,숫자를 입력");
 			}
 		}
-		System.out.println("맞은 횟수"+clear+"틀린 횟수"+fails);
-
+		System.out.println("맞은 횟수 "+clear+" 틀린 횟수 "+fails+" 나머지는 잘못 입력 ");
 	}
 }
